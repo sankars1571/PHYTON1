8c519a789e963848701a25a677a34dab29027b32
sankars <20mp1571@rit.ac.in>
2021-03-08 22:23:13 +0530

    labcycle1

---
+++ py1.1.5.py
@@ -0,0 +1,3 @@
+x=input("enter a word:")
+for i in x:
+            print("ordinal value of ",i,"is ",ord(a))
---
+++ python1.1.3.py
@@ -0,0 +1,9 @@
+list=[10,20,30,40]
+print("list elements: ")
+print(list)
+list1=[]
+print("square list: ")
+for i in list:
+             i=i*i
+             list1.append(i)
+print(list1)
